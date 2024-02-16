16d9023644e582847dffbb5ba58e11ad5bd5335d
amerkhanfar <amer@oplus.ae>
2024-02-16 12:40:53 +0400

    first commit

---
+++ app/instructions/2/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hfgh
+    </div>
+  )
+}
+
+export default page
---
+++ app/instructions/3/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      sadsad
+    </div>
+  )
+}
+
+export default page
